b3af98a501b4805141a2d1712393898a7b5fd4ca
LordFarquhar <65400619+LordFarquhar@users.noreply.github.com>
2020-09-13 19:36:44 +0100

    updated tick-render engine
    more precise tick and render times
    user controlled tick and render target (fps / tps)
    correct fps and tps callouts

--- MAIN.py
+++ MAIN.py
@@ -48,7 +48,7 @@ class Window():
         self.name = name  # screen title displayed
 
         self.resizable = resizable  # whether the screen can be resized by the user
-        
+
         # flags the user should be able to use
         # pygame.FULLSCREEN    create a fullscreen display
         # pygame.RESIZABLE     display window should be sizeable
@@ -105,38 +105,37 @@ class Window():
         pygame.display.set_caption(self.name)
 
     # user start gameloop
-    def startInternalGameLoop(self):
+    def startInternalGameLoop(self, targetFps, targetTps):
         # setup default variables for timing
-        SPT = 1.0/20.0  # seconds per tick
-        SPR = 1.0/60 # seconds per render
+        SPT = 1.0/float(targetTps)  # seconds per tick
+        SPR = 1.0/float(targetFps) # seconds per render
         last_time = 0
         accum_render_time = 0
         accum_tick_time = 0
         fps = 0
         # infinite loop
         while True:
-            start_time = time.time()  # start time of the loop
-            end_time = time.perf_counter() # end time of the loop
-            delta = end_time - last_time # calculate the difference in time between each loop
-            last_time = end_time # set the last time of the loop so the next loop can access it
+            start_time = time.perf_counter() # start time of the loop
+            delta = start_time - last_time # calculate the difference in time between each loop
 
             # tick
             accum_tick_time += delta # if the difference in time between the two loops was more than the number of ticks per second then call multiple ticks
             while accum_tick_time >= SPT:
-                self._tick(delta) # call the internal tick method
+                tps = 1.0 / accum_tick_time
+                self._tick(accum_tick_time, tps) # call the internal tick method
                 accum_tick_time -= SPT # reduce the accumulated time by the intended seconds per tick
 
             # render
             accum_render_time += delta # if the difference in time between the two loops was more than the number of ticks per second then call multiple ticks
             while accum_render_time >= SPR:
-                if (time.time() - start_time) > 0:
-                    # FPS = 1 / time to process loop
-                    fps = 1.0 / (time.time() - start_time)
-                self._render(fps) # call the internal render method
+                fps = 1.0 / accum_render_time
+                self._render(accum_render_time, fps) # call the internal render method
                 accum_render_time -= SPR # reduce the accumulated time by the intended seconds per render
 
+            last_time = start_time # set the last time of the loop so the next loop can access it
+
     # internal tick method
-    def _tick(self, delta):
+    def _tick(self, delta, tps):
         self.tickCount += 1 # increment the tick count
         for event in pygame.event.get():
             # detect screen close
@@ -161,12 +160,12 @@ class Window():
             if event.type == pygame.KEYUP:
                 self._keyUp(event.key) # call the internal keyup method
 
-        self.boundTick(delta) # call the user defined tick method
+        self.boundTick(delta, tps) # call the user defined tick method
 
-    def _render(self, fps):
+    def _render(self, delta, fps):
         self.frameCount += 1 # increment the framecount
         self.windowSurface.fill(Colour.White) # automatically blank the screen
-        self.boundRender(fps) # call the user defined render method
+        self.boundRender(delta, fps) # call the user defined render method
         pygame.display.flip() # refresh the necessary parts of the screen (more efficient than full update)
 
     def _mouseDown(self, button, pos):
@@ -191,22 +190,24 @@ class Window():
 
 
 class Text():
-    def __init__(self, font, fontsize, words, colourRGB, x, y):
+    def __init__(self, font, fontsize, words, colourRGB):
         self.font = font  # the font
         self.fontsize = fontsize  # the size of the font
         self.words = words  # the actual text
         self.colour = colourRGB  # colour in form (R,G,B)
-        self.x = x  # the x coordinate for the screen
-        self.y = y  # the y coordinate for the screen
 
         self.font = pygame.font.SysFont(
             self.font, self.fontsize)  # gets the font
         # creates the text in the font required
         self.text = self.font.render(self.words, True, self.colour)
         self.textRect = self.text.get_rect()  # Draws box around text for alignment
+
+    def render(self, window, x, y):
         self.textRect.left = x  # sets x coord
         self.textRect.top = y  # sets y coord
-
-    def showText(self, window):
         # blits the text to the screen
         window.windowSurface.blit(self.text, self.textRect)
+    
+    def getDimensions(self):
+        width, height = self.text.get_width(), self.text.get_height()
+        return width, height
--- test.py
+++ test.py
@@ -1,19 +1,28 @@
 from MAIN import *
 
+# print(time.perf_counter())
+# print(time.perf_counter_ns())
+
 # create the window
 w = Window(500, 500, "Hello World")
 # initialise the window
 w.initScreen()
 
-def tick(dt):
-    print(f"tick {dt} since last")
+def tick(delta, tps):
+    print(f"{tps:.2f}tps {delta:.10f}delta")
 
-def render(fps):
-    print(f"render {fps}fps")
+def render(delta, fps):
+    print(f"{fps:.2f}fps {delta:.10f}delta")
+    t.render(w, (w.width / 2) - (t.getDimensions()[0] / 2) + w.tickCount, (w.height / 2) - (t.getDimensions()[1] / 2))
 
 # bind events to the user functions
 w.bindTick(tick)
 w.bindRender(render)
 
+
+t = Text("comicsansms", 40, "Hello World", Colour.Blue)
+
+
 # start the gameloop
-w.startInternalGameLoop()
+w.startInternalGameLoop(100, 20)
+

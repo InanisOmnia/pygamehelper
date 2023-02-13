1e936c2b57d7b423f9c09f1a8dbd49464126b0e4
LordFarquhar <65400619+LordFarquhar@users.noreply.github.com>
2023-02-13 11:04:51 +0000

    Update test.py

--- test.py
+++ test.py
@@ -7,21 +7,29 @@ w = pgh.Window((500, 500), "Hello World", [window_flags.resizable])
 w.initScreen()
 
 def tick(delta, tps):
-    print(f"{tps:.2f}tps {delta:.10f}delta")
+    # print(f"{tps:.2f}tps {delta:.10f}delta")
     pass
 
 def render(delta, fps):
-    print(f"{fps:.2f}fps {delta:.10f}delta")
+    # print(f"{fps:.2f}fps {delta:.10f}delta")
     t.render(w, (w.width / 2) - (t.getDimensions()[0] / 2) + w.tickCount, (w.height / 2) - (t.getDimensions()[1] / 2))
     b.render(w.windowSurface)
 
 def mousedown(key, pos):
-    print(f"key {key} at pos {pos}")
+    print(f"mousedown {key} at pos {pos}")
+
+def mouseup(key, pos):
+    print(f"mouseup {key} at pos {pos}")
+
+def keydown(key):
+    print(f"keydown {key}")
 
 # bind events to the user functions
 w.bindTick(tick)
 w.bindRender(render)
 w.bindMouseDown(mousedown)
+w.bindMouseUp(mouseup)
+w.bindKeyDown(keydown)
 
 t = pgh.Text("comicsansms", 40, "Hello World", colours.Blue)
 b = pgh.Button_Circle((80, 80), 50, w, "button")
